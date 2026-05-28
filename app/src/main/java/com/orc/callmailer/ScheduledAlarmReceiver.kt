9c11925b4331f93b14233ff49ce74753a317f885
CountryBlue <69350598+lwg2105@users.noreply.github.com>
2026-05-28 16:15:06 +0900

    feat: update app/src/main/java/com/orc/callmailer/ScheduledAlarmReceiver.kt

---
+++ app/src/main/java/com/orc/callmailer/ScheduledAlarmReceiver.kt
@@ -0,0 +1,15 @@
+package com.orc.callmailer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+
+class ScheduledAlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        ContextCompat.startForegroundService(
+            context,
+            Intent(context, ScheduledUploadService::class.java)
+        )
+    }
+}
